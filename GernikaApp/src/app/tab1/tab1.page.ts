24c8a91f3aaa7a441e4fd5658bbaf09225ae9f7c
Julen Peláez <pelaez.julen@uni.eus>
2025-01-30 10:47:19 +0100

    feat(#14):Responsive mobilarekin

--- GernikaApp/src/app/tab1/tab1.page.ts
+++ GernikaApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,8 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  constructor(private router: Router) {}
+  goMap(){
+    this.router.navigateByUrl('/tabs/mapa');
+  }
 }
